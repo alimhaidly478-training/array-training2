49e5d145894843cb8a3447b176153324273ab2ea
alimhaidly478-training <alimhaidly478@gmail.com>
2026-06-18 11:11:30 +0300

    adding reverse array function

--- main.c++2.cpp
+++ main.c++2.cpp
@@ -1,35 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int countOccurrences(int arr[],int size,int target){
-    int count=0;
-    for (int i=0;i<size;i++){
-        if(arr[i]==target){
-            count++;
-        }
+void reverseArray(int arr[],int size) {
+    int start=0;
+    int end=size-1;
+    
+    while (start<end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
     }
-    return count;
 }
 
 int main() {
-    cout<<"1D Array Practice Round 2"<<endl;
-    
-    const int SIZE=6;
+    cout<<"1D Array training: Reversing"<<endl;
+    const int SIZE=5;
     int myNumbers[SIZE];
-    
-    cout << "Enter " << SIZE << " integers:" << endl;
-    for (int i = 0; i < SIZE; i++) {
-        cout<<"Element"<< i <<":";
+    cout<<"Enter "<<SIZE<<" integers:"<<endl;
+    for(int i=0; i<SIZE;i++){
+        cout<< "Element "<<i<<":";
         cin>>myNumbers[i];
     }
-    
-    int target;
-    cout<<"Enter a number to search for:";
-    cin>>target;
-    
-
-    int totalCount=countOccurrences(myNumbers,SIZE,target);
-    cout<<"The number "<<target<<" appears "<<totalCount<<" times in the array."<<endl;
+    reverseArray(myNumbers,SIZE);
+    cout<<"The reversed array is:"<<endl;
+    for(int i=0;i<SIZE;i++){
+        cout<<myNumbers[i]<<" ";
+    }
+    cout<<endl;
     
     return 0;
 }
